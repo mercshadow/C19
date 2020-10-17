067bbabbe4122f596bf195a3d0aa1800044cb3fe
mercshadow <73010331+mercshadow@users.noreply.github.com>
2020-10-17 10:26:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  rect(10,150,10,100);
+  fill("blue");
+  ellipse(50,50,20,20);
+   ellipse(100,100,30,30);
+   ellipse(200,200,50,50);
+}
